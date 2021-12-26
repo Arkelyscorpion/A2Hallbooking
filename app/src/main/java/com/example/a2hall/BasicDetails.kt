7ae0d80a756cef4014f5c95fceeb32185d09625a
arkelyscorpion <krishnasameer51@gmail.com>
2021-12-26 18:52:55 +0530

    First Commit

---
+++ app/src/main/java/com/example/a2hall/BasicDetails.kt
@@ -0,0 +1,11 @@
+package com.example.a2hall
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BasicDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_basic_details)
+    }
+}
