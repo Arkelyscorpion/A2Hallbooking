a228d0f436e27d7591e6a3b4e74c0c6fc7c75b13
Rishi <apprishiategood002@gmail.com>
2021-12-27 10:47:17 +0530

    Added button action

--- app/src/main/java/com/example/a2hall/BasicDetails.kt
+++ app/src/main/java/com/example/a2hall/BasicDetails.kt
@@ -7,5 +7,6 @@ class BasicDetails : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_basic_details)
+
     }
 }
--- app/src/main/java/com/example/a2hall/MainActivity.kt
+++ app/src/main/java/com/example/a2hall/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.a2hall
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+
+        // operations to be performed
+        // when user tap on the button
+        button?.setOnClickListener()
+        {
+            //intent is used to link one page to another
+            val intent = Intent(this, BasicDetails::class.java)
+            // start your next activity
+            startActivity(intent)          }
     }
 }
