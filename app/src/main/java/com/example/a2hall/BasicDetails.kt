88867a0b98e353af6c40dd13ef5f29867df9f115
arkelyscorpion <krishnasameer51@gmail.com>
2021-12-27 14:42:24 +0530

    Added Material Desgin

--- app/src/main/java/com/example/a2hall/BasicDetails.kt
+++ app/src/main/java/com/example/a2hall/BasicDetails.kt
@@ -1,6 +1,6 @@
 package com.example.a2hall
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class BasicDetails : AppCompatActivity() {
--- app/src/main/java/com/example/a2hall/MainActivity.kt
+++ app/src/main/java/com/example/a2hall/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.a2hall
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
@@ -10,10 +10,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val button = findViewById<Button>(R.id.button)
 
-        // operations to be performed
-        // when user tap on the button
+        /*
+        operations to be performed
+        when user tap on the button
+        */
         button?.setOnClickListener()
         {
             //intent is used to link one page to another
