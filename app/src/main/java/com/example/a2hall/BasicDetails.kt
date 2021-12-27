a67ac8c9b3dd6275dbd2ac10f7fdbc378ae930bf
Rishi <apprishiategood002@gmail.com>
2021-12-27 15:39:31 +0530

    Added few more fields and second button action

--- app/src/main/java/com/example/a2hall/BasicDetails.kt
+++ app/src/main/java/com/example/a2hall/BasicDetails.kt
@@ -1,12 +1,27 @@
 package com.example.a2hall
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class BasicDetails : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_basic_details)
 
+        val button = findViewById<Button>(R.id.button2)
+
+        /*
+        operations to be performed
+        when user tap on the button
+        */
+        button?.setOnClickListener()
+        {
+            //intent is used to link one page to another
+            val intent = Intent(this, SecondaryDetails::class.java)
+            // start your next activity
+            startActivity(intent)          }
     }
+
 }
---
+++ app/src/main/java/com/example/a2hall/SecondaryDetails.kt
@@ -0,0 +1,11 @@
+package com.example.a2hall
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondaryDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_secondary_details)
+    }
+}
